cbf1abb901dcfaae72991c99dd5f26fd0993e3e4
flown.lan <flown.lan@gmail.com>
2026-01-14 21:52:53 +0800

    feat: implement Phase 5.1 OCR enhancement with Native Scanner and OpenCV

---
+++ lib/logic/providers/native_plugins_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../services/document_scanner_service.dart';
+import '../services/image_processor_service.dart';
+
+part 'native_plugins_provider.g.dart';
+
+@riverpod
+DocumentScannerService documentScannerService(Ref ref) {
+  return DocumentScannerService();
+}
+
+@riverpod
+ImageProcessorService imageProcessorService(Ref ref) {
+  return ImageProcessorService();
+}
